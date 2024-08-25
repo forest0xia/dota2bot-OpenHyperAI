39dc769da42f1ab2a13dc2ef8e00793d8743639e
Doctor <thirvondukr@gmail.com>
2024-08-26 02:28:51 +0300

    Migrate FunLib/version to ts

--- bots/FunLib/version.lua
+++ bots/FunLib/version.lua
@@ -1,9 +1,5 @@
-local X = { }
-
-X['number'] = "0.7.37b - 2024/8/23"
-
-X['recentChangeLogs'] = {
-    "GLHF"
-}
-
-return X
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+local ____exports = {}
+____exports.number = "0.7.37b - 2024/8/23"
+____exports.recentChangeLogs = {"GLHF"}
+return ____exports
---
+++ bots/FunLib/version.ts
@@ -0,0 +1,2 @@
+export const number = '0.7.37b - 2024/8/23'
+export const recentChangeLogs = ['GLHF']
