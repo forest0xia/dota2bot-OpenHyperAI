cfa280be08e9fb950365ba4ee258041c9130edfa
Forest Xia <myouxia@gmail.com>
2024-10-06 12:25:18 -0700

    Update version

--- bots/FunLib/version.lua
+++ bots/FunLib/version.lua
@@ -1,5 +1,5 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
-____exports.number = "0.7.37c - 2024/09/28"
+____exports.number = "0.7.37c - 2024/10/06"
 ____exports.recentChangeLogs = {"GLHF"}
 return ____exports
--- typescript/bots/FunLib/version.ts
+++ typescript/bots/FunLib/version.ts
@@ -1,5 +1,5 @@
 // Note, only the person responsible for publishing new releases in Valve Workshop should change this version. 
 //  make sure have `npm run dev` running, or run `npm run build` once after modifying this value.
-export const number = "0.7.37c - 2024/09/28"
+export const number = "0.7.37c - 2024/10/06"
 
 export const recentChangeLogs = ["GLHF"]
