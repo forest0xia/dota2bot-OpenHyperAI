483fe05a287fd1ba389a191e1dc4c2e484c93582
Forest Xia <myouxia@gmail.com>
2026-03-29 14:30:09 -0700

    update version

--- bots/FunLib/version.lua
+++ bots/FunLib/version.lua
@@ -1,5 +1,5 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
-____exports.number = "0.7.40 - 2026/03/03"
+____exports.number = "0.7.41 - 2026/03/28"
 ____exports.recentChangeLogs = {"GLHF"}
 return ____exports
